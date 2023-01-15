ebf4bcc0a26acad3421e338d5458fb37b2e5cd26
박은영_웹개발1팀 <eypark@eduwill.net>
2023-01-15 20:24:13 +0900

    포트폴리오

--- js/site.js
+++ js/site.js
@@ -21,15 +21,14 @@
 // window.addEventListener('scroll', onScroll);
 
 $(window).scroll(function () {
-  let scroll = $(window).scrollTop();
+	let scroll = $(window).scrollTop();
 
-  const sec_start1 = $('.main-title').offset().top;
-  
-  if (scroll > sec_start1) {
-    gsap.to(".site_list_title h3", { duration: 0.8, opacity: 1, y: 3, delay: 1.1 });
-  }
+	const sec_start1 = $('.main-title').offset().top;
+
+	if (scroll > sec_start1) {
+		gsap.to(".site_list_title h3", { duration: 0.8, opacity: 1, y: 3, delay: .3 });
+	}
 });
-
 
 //site slider
 // $('.slide-nav').on('click', function (e) {
